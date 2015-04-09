5573bdad421af122767c1392dc4b068c51b66039
keithma <keithma@bu.edu>
2015-04-09 13:32:28 -0400

    Fixed projection of output file
    For some reason, the 'raster' package changes the crs when writing a file to
    disk. I could not change this behavior, and I could not find a way to use rgdal to
    rewrite the crs (without delving into undocumented subroutines). As an
    annoyingly clunky workaround, I currently call an external python script
    (provided as an example for the python GDAL bindings) to match the output and
    input projections after the fact. This script is called from R to avoid adding
    additional files.
    
    Also, I added a shebang line to the script so that it can be called directly
    from the command line, meaning either 'Rscript biomass_model.R' of
    './biomass_model.r' will do the trick.

--- biomass_model.R
+++ biomass_model.R
@@ -1,7 +1,15 @@
+#!/usr/bin/env Rscript
+#
 # Estimation of urban biomass (add details here)
 #
 # Apply linear model to estimate biomass from NBCD, NLCAN, and NLCD data. It is
 # assumed that all input data share teh same extent, projection, and resolution.
+#
+# Notes: 
+#   - The raster package annoyingly changes the CRS when the file is written. To
+#   get around this, an external routine (gdalcopyproj.py) is used to reset it
+#   properly.
+#   - For BU SCC: load the modules R/R-3.1.1, gdal, proj4, and python/2.7.5
 #
 # Brady Hardiman (the main guy)
 # Keith Ma (technical assistance)
@@ -17,7 +25,7 @@ out.type <- 'FLT4S' # see ?dataType
 
 # Define model 
 model <- function(nbcd, nlcan, nlcd) {
-  1.1*nbcd + 0.0*nlcan + 0.0*nlcd  
+  1.0*nbcd + 0.0*nlcan + 0.0*nlcd  
 }
 
 # Load dependencies
@@ -31,5 +39,8 @@ nlcd.r <- raster(nlcd.f)
 # Apply model
 out.r <- overlay(nbcd.r, nlcan.r, nlcd.r, fun = model, filename = out.f, 
                  format = 'GTiff', dataType = 'FLT4S', overwrite = TRUE)
+
+# Fix projection for output
+system(paste('./gdalcopyproj.py', nbcd.f, out.f))
 
 # Done.
